79fc538ee4442298bcada48eed02b35008811dde
wouter.devriese@luca-arts.be <wouter.devriese@luca-arts.be>
2023-08-29 16:25:47 +0200

    added initial datastream files

---
+++ Assets/Scripts/DataStreams/LSL/OutletPassThrough.cs
@@ -0,0 +1,114 @@
+using UnityEngine;
+using UnityEngine.Events;
+using static LSL.liblsl;
+
+namespace LSL
+{
+    [System.Serializable]
+    public class m_LSL_Event : UnityEvent<string>
+    {
+    }
+    public enum Marker { game_start = 0, ball_release = 1, ball_good_hit = 2, ball_bad_hit = 3, score = 4, test = 5, end_game = 6, LSL_test = 7, caregiver_in_view = 8, caregiver_starts_feedback = 9, caregiver_stops_feedback = 10 };
+
+    public class OutletPassThrough : MonoBehaviour
+    {
+        /*
+         * This is a simple example of an LSL Outlet to stream out irregular events occurring in Unity.
+         * This uses only LSL.cs and is intentionally simple. For a more robust version, see another sample.
+         * 
+         * We stream out the trigger event during OnTriggerEnter which is, in our opinison, the closest
+         * time to when the trigger actually occurs (i.e., independent of its rendering).
+         * A simple way to print the events is with pylsl: `python -m pylsl.examples.ReceiveStringMarkers`
+         *
+         * If you are instead trying to log a stimulus event then there are better options. Please see the 
+         * LSL4Unity SimpleStimulusEvent Sample for such a design.
+         */
+
+        #region SingletonPattern
+        private static OutletPassThrough _instance;
+        public static OutletPassThrough Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    Debug.Log("The OutletPassThrough is Null");
+                }
+                return _instance;
+            }
+        }
+        private void Awake()
+        {
+            _instance = this;
+        }
+        #endregion
+
+        [SerializeField] private string StreamName_1 = "DataSyncMarker_emotibit";
+        [SerializeField] private string StreamName_2 = "DataSyncMarker_eeg";
+        [SerializeField] private string ContentType_1 = "Markers";
+        [SerializeField] private string ContentType_2 = "Markers";
+        [SerializeField] private string StreamId_1 = "LSL1";
+        [SerializeField] private string StreamId_2 = "LSL2";
+        [SerializeField] private channel_format_t channel_format_type = channel_format_t.cf_string;
+
+        private StreamOutlet outlet_1;
+        private StreamOutlet outlet_2;
+        private string[] sample = { "" };
+        //private float i = 0.0f;
+
+        void Start()
+        {
+            StreamInfo streamInfo1 = new(StreamName_1, ContentType_1, 1, IRREGULAR_RATE,
+                channel_format_type, StreamId_1); //int8 should be more than enough for coding the differing signals
+            outlet_1 = new StreamOutlet(streamInfo1);
+            StreamInfo streamInfo2 = new(StreamName_2, ContentType_2, 1, IRREGULAR_RATE,
+                channel_format_type, StreamId_2); //int8 should be more than enough for coding the differing signals
+            outlet_2 = new StreamOutlet(streamInfo2);
+        }
+        public void SendMarker(Marker marker)
+        {
+            //Debug.Log(Time.time + (int)marker);
+            //switch (marker)
+            //{
+            //    case Marker.ball_release:
+            //    case Marker.ball_good_hit:
+            //    case Marker.ball_bad_hit:
+            //    case Marker.score:
+            //        SendMarker((int)marker);
+            //        break;
+            //    case Marker.test:
+            //        SendMarker((int)marker);
+            //        break;
+            //    default:
+            //        Debug.Log("unknown command");
+            //        break;
+            //}
+            SendMarker((int)marker);
+        }
+
+        private void SendMarker(int marker)
+        {
+            sample[0] = "" + marker;
+            if (outlet_1 != null)
+            {
+                outlet_1.push_sample(sample);
+                //Debug.Log("push sample outlet 1");
+
+            }
+            if (outlet_2 != null)
+            {
+                outlet_2.push_sample(sample);
+                //Debug.Log("push sample outlet 2");
+
+            }
+            //Debug.Log("LSL stream: " + Time.time + " " + sample[0]);
+
+        }
+
+        public void LSLTest()
+        {
+            Debug.Log("Sending LSL test");
+            SendMarker(Marker.LSL_test);
+        }
+    }
+}
---
+++ Assets/Scripts/DataStreams/Websocket/WsClient.cs
@@ -0,0 +1,181 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using WebSocketSharp;
+
+public class WsClient : MonoBehaviour
+{
+    #region SingletonPattern
+    private static WsClient _instance;
+    public static WsClient Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.Log("The WsClient is Null");
+            }
+            return _instance;
+        }
+    }
+    private void Awake()
+    {
+        _instance = this;
+    }
+    #endregion
+
+    public WebSocket ws;
+    public bool hasWsConnection = false;
+    private float timeoutLength = 2.0f;
+    private float timeoutTimer = 0.0f;
+    [SerializeField] private string ip = "localhost";
+    [SerializeField] private string port = "8080";
+    //[SerializeField] private Ws_to_debug wsToDebug;
+    public PlayerVals playerVals;
+
+    [System.Serializable] public class WsEvent : UnityEvent<string> { }
+    //public WsEvent wsMsgReceived;
+    public UnityEvent wsMsgReceived;
+    public class WSHelloworld
+    {
+        public string connectMessage;
+        public string _time;
+        public WSHelloworld(string connectmsg)
+        {
+            connectMessage = connectmsg;
+            _time = System.DateTime.Now.ToString("f");
+        }
+        public string HelloMessage()
+        {
+            return JsonUtility.ToJson(this);
+        }
+    }
+    private WSHelloworld wsHello;
+
+
+    private void Start()
+    {
+        wsHello = new WSHelloworld("Hello from " + ip);
+        //wsToDebug = this.GetComponent<Ws_to_debug>();
+        ws = new WebSocket("ws://" + ip + ":" + port);
+        ws.ConnectAsync();
+
+        ws.OnOpen += (sender, e) =>
+        {
+            hasWsConnection = true;
+            var wsmsg = wsHello.HelloMessage();
+            SendWSMessage(wsmsg);
+            wsMsgReceived?.Invoke();
+
+        };
+
+        ws.OnMessage += (sender, e) =>
+        {
+            Debug.Log("Message Received from " + ((WebSocket)sender).Url + ", Data : " + e.Data);
+            HandleIncomingMessage(e.Data);
+        };
+        ws.OnClose += (sender, e) =>
+        {
+            //Debug.Log("the connection did close");
+            hasWsConnection = false;
+        };
+    }
+
+    private void Update()
+    {
+        // we check for a websocket connection and otherwise try to reconnect every <timeoutLength> seconds.
+        if (ws == null)
+        {
+            return;
+        }
+        if (hasWsConnection == false)
+        {
+            timeoutTimer += Time.deltaTime;
+            if (timeoutTimer > timeoutLength)
+            {
+                ws.ConnectAsync();
+                timeoutTimer = 0;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                ws.Send("test");
+            }
+        }
+    }
+
+    private void HandleIncomingMessage(string message)
+    {
+        //Debug.Log("INVOKING " + message);
+        message = message.Replace("\'", "\""); // python websocket uses single quotes?
+        WebsocketMessage res = JsonUtility.FromJson<WebsocketMessage>(message);
+        switch (res.type)
+        {
+            case "player":
+                break;
+            default:
+                Debug.LogWarning("unknown command");
+                break;
+        }
+        Debug.Log(message);
+        //wsMsgReceived.Invoke(message);
+        // TODO
+    }
+
+
+    public void SendWSMessage(string message)
+    {
+        //Debug.Log("sending " + message);
+        DateTime epochStart = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        int cur_time = (int)(DateTime.UtcNow - epochStart).TotalSeconds;
+        if (hasWsConnection)
+        {
+            ws.Send("time: " + cur_time + ", " + message);
+        }
+    }
+    public void SetDebug(string message)
+    {
+        Debug.Log("printing " + message);
+        if (message is null)
+        {
+            throw new ArgumentNullException(nameof(message));
+        }
+
+    }
+
+    private void OnApplicationQuit()
+    {
+
+        DateTime epochStart = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        int cur_time = (int)(DateTime.UtcNow - epochStart).TotalSeconds;
+        SendWSMessage("time: " + cur_time + ", message: Android application is closing ");
+        ws.CloseAsync(CloseStatusCode.Normal);
+    }
+
+
+}
+[Serializable]
+public class PlayerVals
+{
+    public string name;
+    public int height;
+    public string gender;
+    public int contingency;
+    public int trial_block;
+    public bool valuesSet = false;
+    public string SaveToString()
+    {
+        return JsonUtility.ToJson(this);
+    }
+}
+
+[Serializable]
+public class WebsocketMessage
+{
+    public string type;
+    public string data;
+    public PlayerVals playerValues;
+    //TODO
+}
